95c4bdc9682e86e79c1cf949b2dbe59e6c432c26
NuvandaPV <jp@hhmn.de>
2018-06-05 17:47:30 +0200

    Add script.js again

---
+++ scripts/script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+/*
+ * Additional scripts.
+ *
+ * This script implements additional functionality not provided by Squarespace.
+ */
+
+/*
+ * Initial setup.
+ */
+(function() {
+
+})();
+
+/*
+ * Main script.
+ */
+window.onload = function() {
+
+};
