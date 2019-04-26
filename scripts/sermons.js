de7bafa286a908bd17da3da414cce98d523bcd7f
NuvandaPV <jp@hhmn.de>
2019-04-26 23:51:03 +0200

    Add sermons.js.

---
+++ scripts/sermons.js
@@ -0,0 +1,20 @@
+/*
+ * Sermons page.
+ *
+ * This script implements functionality to quickly list and download sermons.
+ *
+ * TODO This is a stub
+ */
+
+/*
+ * Initial setup.
+ */
+(function() {
+    "use strict";
+
+    /*
+     * Main script.
+     */
+    $(document).ready(function() {
+    });
+})();
